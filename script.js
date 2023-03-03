8f06863a5a227474e83901da6913b56a07983b65
Krzysztof Walczak <christopher.walczak@gmail.com>
2023-03-03 18:30:32 +0000

    Add Section 2 in the HTML file. Create images folder and upload content

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.querySelector('.menu').addEventListener('click', () => {
-	document.querySelectorAll('.target').forEach((item) => {
-		item.classList.toggle('change');
-	})
-})
-
 const icons = document.querySelectorAll('.section-1-icons i')
 let i = 1
 
